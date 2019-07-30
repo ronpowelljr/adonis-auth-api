2eb6c7ef1540a7f3052ea044add5dae96eade813
Ron Powell <ron.powell.jr@gmail.com>
2019-07-30 11:07:33 -0700

    initial commit

---
+++ app/Models/Post.js
@@ -0,0 +1,10 @@
+
+//label app/Models/Post.js
+'use strict'
+const Model = use('Model')
+class Post extends Model {
+user() {
+return this.belongsTo('App/Models/User');
+ }
+}
+module.exports = Post
